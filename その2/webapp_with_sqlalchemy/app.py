a888d86fc56aad471403c59e784e1b4e1668cfea
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-08-08 16:41:31 +0900

    complete

--- その2/webapp_with_sqlalchemy/app.py
+++ その2/webapp_with_sqlalchemy/app.py
@@ -11,35 +11,39 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), index = True, unique = True)
-    description = db.Column(db.String(120), index = True, unique = True)
-    user_image_url = db.Column(db.String(120), index = True, unique = True)
-    date_published = db.Column(db.DateTime, defalut = datetime.utcnow)
-    def __repr__(self):
-        return '<User %r>' % self.username
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    description = db.Column(db.String(120))
+    user_image_url = db.Column(db.String(120))
+    date_published = db.Column(db.DateTime, default=datetime.utcnow)
+    # def __repr__(self):
+    #     return '<User %r>' % self.username
         
         
 @app.route('/')
 def index():
     users = User.query.all()
-    return render_template('index.html', users = users)
+    return render_template('index.html', users=users)
 
 @app.route('/form')
 def form():
     return render_template('form.html')
 
-@app.route('/register', methods = ['POST'])
+@app.route('/register', methods=['POST'])
 def register():
     if request.form['username'] and request.form['description'] and request.files['image']:
         f = request.files['image']
         filepath = 'static/' + secure_filename(f.filename)
         f.save(filepath)
         filepath = '/' + filepath
-        newUser = User(username = request.form['username'], description = request.form['description'], user_image_url = filepath)
+        newUser = User(username=request.form['username'], description=request.form['description'], user_image_url=filepath)
         db.session.add(newUser)
         db.session.commit()
-        return render_template('result.html', username = request.form['username'], description = request.form['description'])
+        return render_template('result.html', username=request.form['username'], description=request.form['description'])
     else:
         return render_template('error.html')
+
+@app.cli.command('initdb')
+def initdb_command():
+    db.create_all()
         
