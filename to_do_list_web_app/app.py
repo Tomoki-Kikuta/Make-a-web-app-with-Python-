0a8e8966043ca4e63b622a895648f7abcacee982
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-10-26 15:32:02 +0900

    study how to do login

--- to_do_list_web_app/app.py
+++ to_do_list_web_app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, Response, abort, render_template
+from flask import Flask, request, Response, abort, render_template, redirect, url_for
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
 from collections import defaultdict
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.utils import secure_filename
 from datetime import datetime
+
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -15,6 +16,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/testdb"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -23,9 +25,10 @@ class User(UserMixin, db.Model):
     #     return '<User %r>' % self.username
 
 
+
 @login_manager.user_loader
 def load_user(user_id):
-    return user.get(user_id)
+    return 
 
 
 @app.route('/')
@@ -39,8 +42,8 @@ def register():
         # ログイン用ユーザー作成
         return render_template("new_register.html")
     else:
-        if request.form['username'] and request.form['password']:
-            newUser = User(username=request.form['username'], password=request.form["password"])
+        if request.form['username'] and request.form['password'] and request.form['id']:
+            newUser = User(id=request.form['id'], username=request.form['username'], password=request.form["password"])
             db.session.add(newUser)
             db.session.commit()
             return render_template('result.html', username=request.form['username'])
@@ -54,10 +57,11 @@ def login():
         # ユーザーチェックをする
         users = User.query.all()
         for user in users:
+            # TODO: databaseから効率的に取得する方が良い
             if user.username == request.form["username"] and user.password == request.form["password"]:
                 # ユーザーが存在した場合はログイン
-                login_user(user.id, users)
-                return render_template("form.html")
+                login_user(user)
+                return redirect(url_for("protected", user=user.username))
         return render_template("error.html")
     else:
         return render_template("login.html")
@@ -66,18 +70,22 @@ def login():
 # ログアウトパス
 @app.route('/logout/')
 @login_required
-def logout(user):
-    logout_user(user.id, user)
+def logout():
+    logout_user()
     return render_template("logout.html")
 
 
 # ログインしないと表示されないパス
-@app.route('/protected/')
+@app.route('/protected/<user>/')
 @login_required
-def protected():
-    return render_template("form.html")
+def protected(user):
+    return render_template("form.html", username=user)
 
 
 @app.cli.command('initdb')
 def initdb_command():
     db.create_all()
+
+
+if __name__=='__main__':
+    app.run(debug=False)
